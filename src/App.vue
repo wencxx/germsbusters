2a60c757313d9b728b18228506064cccf6ed08e7
wenc <wncbtrnfl@gmail.com>
2024-12-17 12:19:13 +0800

    fixed sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="h-screen w-screen flex font-inter">
     <sidebarComponent v-if="isAuthenticated && showSidebar" class="w-full max-w-xs border-r h-full p-5 duration-150 hidden lg:block" />
-    <sidebarComponent v-if="isAuthenticated && showSidebar" class="w-full max-w-xs border-r h-full p-5 duration-150 fixed left-0 top-0 z-20 bg-white block lg:hidden" />
-    <Icon v-if="showSidebar" icon="mdi:menu" class="text-3xl cursor-pointer block lg:hidden absolute top-5 left-3/4 -translate-x-10 z-50" @click="showSidebar = !showSidebar" />
+    <sidebarComponent v-if="isAuthenticated && !showSidebar" class="w-full max-w-xs border-r h-full p-5 duration-150 fixed left-0 top-0 z-20 bg-white block lg:hidden" />
+    <Icon v-if="!showSidebar" icon="mdi:menu" class="text-3xl cursor-pointer block lg:hidden absolute top-5 left-3/4 -translate-x-10 z-50" @click="showSidebar = !showSidebar" />
     <div class="w-full flex flex-col">
       <headerComponent v-if="isAuthenticated" class="w-full h-[7dvh] border-b bg-gray-100" @toggleSidebar="showSidebar = !showSidebar" />
       <router-view class="h-full w-full" :class="{ '!h-[93dvh] bg-gray-100 px-5': isAuthenticated }"/>
